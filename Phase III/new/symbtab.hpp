03029d15b3782b42f10ab36b034147e050de97de
compilers <harshapranav323@gmail.com>
2023-11-08 16:14:34 +0530

    symtabl crct file updated, vardec and functions

--- Phase III/new/symbtab.hpp
+++ Phase III/new/symbtab.hpp
@@ -5,7 +5,7 @@ extern char* yytext;
 // string scope;
 bool q;
 string type;
-bool is_function = false;
+bool is_func_bool = false;
 
 
 struct sym_tab_entries {
@@ -14,8 +14,22 @@ struct sym_tab_entries {
   string type;
   // string scope;
 };
+typedef sym_tab_entries sym_tab_entries;
 vector<sym_tab_entries*> symbol_table;
 
+struct var_records{
+  string name;
+  string type;
+};
+typedef var_records var_records;
+vector <var_records*> var_list;
+
+struct par_records{
+  string name;
+  string type;
+};
+typedef par_records par_records;
+vector <par_records*> par_list;
 
 struct function_records{
   string name;
@@ -23,50 +37,62 @@ struct function_records{
   int num_of_param;
   // string scope;
   int ret_counter;
-  vector<par_records> par_list; 
-  vector<var_records> var_list;
+  vector<par_records*> par_list; 
+  vector<var_records*> var_list;
 };
+typedef function_records function_records;
 vector <function_records*> function_sym_table;
 
 
-struct var_records{
-  string name;
-  string type;
-};
-
-vector <var_records*> var_list;
-
-
-struct par_records{
-  string name;
-  string type;
-};
-
-vector <par_records*> par_list;
-
-
-void new_func_entry(string name, string result_type,int num_of_param, vector<par_records> par_list,vector<var_records>var_list){
-  struct function_records* temp = new function_records;
-  // temp->name = name;
-  // temp->result_type = result_type;
-  // temp->num_of_param = num_of_param;
-  // temp->par_list = par_list;
-  // temp->var_list = var_list;
+void new_func_entry(string name, string result_type,int num_of_param, vector<par_records*> par_list,vector<var_records*>var_list){
+  function_records* temp = new function_records;
+  temp->name = name;
+  temp->result_type = result_type;
+  temp->num_of_param = num_of_param;
+  temp->par_list = par_list;
+  temp->var_list = var_list;
   // temp->scope = scope;
-  function_sym_table.push_back({name,result_type,num_of_param,par_list,var_list});
+  // function_sym_table.push_back({name,result_type,num_of_param,par_list,var_list});
+  function_sym_table.push_back(temp);
 }
 
 void new_entry(string id_name,string data_type, string type) {
 	struct sym_tab_entries* temp = new sym_tab_entries;
-  // temp->id_name = id_name;
-	// temp->data_type = data_type;
-	// temp->type = type;
+  cout<<"id: "<<id_name<<endl;
+  temp->id_name = id_name;
+	temp->data_type = data_type;
+	temp->type = type;
 	// temp->scope = scope;
-  symbol_table.push_back({id_name,data_type,type});
+  // symbol_table.push_back({id_name,data_type,type});
+  symbol_table.push_back(temp);
 }
 
 void insert_type() {
 	type = yytext;
+}
+
+bool search_in_symtab(string name) { 
+  int i; 
+  if(is_func_bool){
+    for(int i = 0; i < par_list.size(); i++) {
+      if(par_list[i]->name == name) {   
+        return false;
+      }
+    }
+    for(i = 0; i < var_list.size(); i++) {
+      if(var_list[i]->name == name) {   
+        return false;
+      }
+   }
+  }
+  else {
+    for(i = 0; i < symbol_table.size(); i++) {
+      if(symbol_table[i]->id_name == name) {   
+        return false;
+      }
+    }
+  } 
+  return true;
 }
 
 void add(char c) {
@@ -90,37 +116,13 @@ void add(char c) {
   }
 }
 
-bool search_in_symtab(string name) { 
-  int i; 
-  if(is_function){
-    for(int i = 0; i < par_list.size(); i++) {
-      if(par_list[i]->name == name) {   
-        return false;
-      }
-    }
-    for(i = 0; i < var_list.size(); i++) {
-      if(var_list[i]->name == name) {   
-        return false;
-      }
-   }
-  }
-  else {
-    for(i = 0; i < symbol_table.size(); i++) {
-      if(symbol_table[i]->id_name == name) {   
-        return false;
-      }
-    }
-  } 
-  return true;
-}
-
-
 void print_table(){
   printf("\n\n");
 	printf("\nNAME   DATATYPE   TYPE   SCOPE \n");
 	printf("_______________________________________\n\n");
 	for(int i = 0; i< symbol_table.size(); i++) {
-		printf("%s\t%s\t%s\t%d\t\n", symbol_table[i]->id_name, symbol_table[i]->data_type, symbol_table[i]->type);
+    cout<< symbol_table[i]->id_name<<" "<<symbol_table[i]->data_type<<" "<<symbol_table[i]->type<<endl;
+		// printf("%s\t%s\t%s\t%d\t\n", symbol_table[i]->id_name, symbol_table[i]->data_type, symbol_table[i]->type);
 	}
 
 }
